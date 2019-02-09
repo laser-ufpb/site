66fa8a30c285dfa306110e0138384ddbb8ee0f4c
brunobruck <bruno.p.bruck@gmail.com>
2019-02-09 19:08:57 -0300

    new data

--- js/publications.js
+++ js/publications.js
@@ -2,17 +2,25 @@ $(document).ready(function(){
     
     // ----------------------------
     // Setup the publications table
-    $('#table-publications').DataTable({
-        "ordering": false,
+    var table = $('#table-publications').DataTable({
+        "ordering": true,
+        "order": [[ 2, "desc" ]],
         "info": false,
-        "searching": false,
+        "searching": true,
         "lengthChange": false,
         "language": {
             "paginate": {
                 "previous": "Anterior",
                 "next": "Próxima"
-            }
+            },
+            "search": "Busca"
           } 
     });
+
+    table.on( 'order.dt search.dt', function () {
+        table.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
     
 });
