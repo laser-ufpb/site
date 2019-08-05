fdd3e5858f9e74ff82d32bf539ebb819db0952ae
Bruno <bruno@Mordor-3.local>
2019-08-05 12:52:10 -0300

    news update

--- js/core.js
+++ js/core.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+    $('.moment-date').each(function(){
+        var date = $(this).text();
+        moment.locale('pt-br');
+        $(this).text(moment(date, "YYYY-MM-DD HH:mm").fromNow());
+    });
 });
